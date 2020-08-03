a3d63f44acc5f7d353a5f914f95b3d1e278bdd4e
Explodeus <volodya.sokha@gmail.com>
2020-08-03 11:16:47 +0300

    Added world defining

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict"
 
+// Reading the input from the user that initiated the process.
+
+let WWidth  = process.argv[2] || 30;
+let WHeight = process.argv[3] || 10;
+
 // Constants defined that render the world.
 
 const WC = '+'; // world corner
@@ -10,3 +15,13 @@ const SH = 'O'; // snake head
 const SB = 'o'; // snake body
 const SF = '$'; // snake food
 const SC = '*'; // snake collision
+
+// Define the World
+
+let world = [];
+for (let row = 0; row < WHeight; row++) {
+  world[row] = [];
+  for (let col = 0; col < WWidth; col++) {
+    world[row][col] = WS;
+  }
+}
