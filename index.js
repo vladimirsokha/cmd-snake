42790755219b5de2ed1c50cdc808d6979a8dcadc
Explodeus <volodya.sokha@gmail.com>
2020-08-03 11:15:14 +0300

    index file & snake interface symbols added

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// Constants defined that render the world.
+
+const WC = '+'; // world corner
+const WV = '|'; // world vertical wall (edge)
+const WH = '-'; // world horizontal wall (edge)
+const WS = ' '; // world space (a space character)
+const SH = 'O'; // snake head
+const SB = 'o'; // snake body
+const SF = '$'; // snake food
+const SC = '*'; // snake collision
